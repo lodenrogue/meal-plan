9ebeaeb71515c71ff1b8fc0e6665032b92094463
lodenrogue <lodenrogue@gmail.com>
2021-03-24 17:57:15 -0400

    fixed tabs and remove pycache

--- src/menu/menu.py
+++ src/menu/menu.py
@@ -3,39 +3,38 @@ from menu.lunch_menu import LunchMenu
 from menu.snack_menu import SnackMenu
 from menu.dinner_menu import DinnerMenu
 
-
 class Menu():
 
-	def __init__(self, calories, menu_type):
-		self.calories = int(calories)
-		self.menu_type = menu_type
+    def __init__(self, calories, menu_type):
+        self.calories = int(calories)
+        self.menu_type = menu_type
 
-		self.lunch_menu = LunchMenu()
-		self.snack_menu = SnackMenu()
-		self.dinner_menu = DinnerMenu(menu_type)
-
-
-	def get_lunch(self):
-		return self.lunch_menu.get_menu()
+        self.lunch_menu = LunchMenu()
+        self.snack_menu = SnackMenu()
+        self.dinner_menu = DinnerMenu(menu_type)
 
 
-	def get_dinner(self):
-		lunch = self.get_lunch()
-		snack = self.get_snack()
-		available_calories = self.calories - self._add_calories(lunch, snack)
-
-		return self.dinner_menu.get_menu(available_calories)
+    def get_lunch(self):
+        return self.lunch_menu.get_menu()
 
 
-	def get_snack(self):
-		return self.snack_menu.get_menu()
+    def get_dinner(self):
+        lunch = self.get_lunch()
+        snack = self.get_snack()
+        available_calories = self.calories - self._add_calories(lunch, snack)
+
+        return self.dinner_menu.get_menu(available_calories)
 
 
-	def _add_calories(self, meal1, meal2):
-		meal1_calories = self._get_total_calories(meal1)
-		meal2_calories = self._get_total_calories(meal2)
-		return meal1_calories + meal2_calories
+    def get_snack(self):
+        return self.snack_menu.get_menu()
 
 
-	def _get_total_calories(self, meal):
-		return sum(item['calories'] for item in meal)
+    def _add_calories(self, meal1, meal2):
+        meal1_calories = self._get_total_calories(meal1)
+        meal2_calories = self._get_total_calories(meal2)
+        return meal1_calories + meal2_calories
+
+
+    def _get_total_calories(self, meal):
+        return sum(item['calories'] for item in meal)
--- src/plan.py
+++ src/plan.py
@@ -11,36 +11,36 @@ from menu.menu import Menu
 
 
 def run(calories, menu_type, is_html):
-	menu = Menu(calories, menu_type)
-	lunch = menu.get_lunch()
-	snack = menu.get_snack()
-	dinner = menu.get_dinner()
+    menu = Menu(calories, menu_type)
+    lunch = menu.get_lunch()
+    snack = menu.get_snack()
+    dinner = menu.get_dinner()
 
-	if is_html:
-		display_html_table(lunch, dinner, snack)
-	else:
-		display_rich_table(lunch, dinner, snack)
+    if is_html:
+        display_html_table(lunch, dinner, snack)
+    else:
+        display_rich_table(lunch, dinner, snack)
 
 
 def display_html_table(lunch, dinner, snack):
-	lunch_table = HtmlTable(lunch).create_table('Lunch')
-	dinner_table = HtmlTable(dinner).create_table('Dinner')
-	snack_table = HtmlTable(snack).create_table('Snacks')
+    lunch_table = HtmlTable(lunch).create_table('Lunch')
+    dinner_table = HtmlTable(dinner).create_table('Dinner')
+    snack_table = HtmlTable(snack).create_table('Snacks')
 
-	print(lunch_table)
-	print(dinner_table)
-	print(snack_table)
+    print(lunch_table)
+    print(dinner_table)
+    print(snack_table)
 
 
 def display_rich_table(lunch, dinner, snack):
-	lunch_table = RichTable(lunch).create_table('Lunch')
-	dinner_table = RichTable(dinner).create_table('Dinner')
-	snack_table = RichTable(snack).create_table('Snacks')
+    lunch_table = RichTable(lunch).create_table('Lunch')
+    dinner_table = RichTable(dinner).create_table('Dinner')
+    snack_table = RichTable(snack).create_table('Snacks')
 
-	console = Console()
-	console.print(lunch_table)
-	console.print(dinner_table)
-	console.print(snack_table)
+    console = Console()
+    console.print(lunch_table)
+    console.print(dinner_table)
+    console.print(snack_table)
 
 
 def get_args():
